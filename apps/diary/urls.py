6246d47289741c5d53990b91f9fdcd872a4b1598
xnor <xnor@example.com>
2026-08-14 15:51:22 +0300

    Щоденник

--- apps/diary/urls.py
+++ apps/diary/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'diary'
 
 urlpatterns = [
+    path('', views.DiaryOverviewView.as_view(), name='overview'),
+    path('student/<int:pk>/', views.StudentGradesView.as_view(), name='student_detail'),
 ]
--- apps/diary/views.py
+++ apps/diary/views.py
@@ -1,1 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Avg
+from django.views import generic
+
+from .models import StudentProfile, Subject, Grade
+
+
+class DiaryOverviewView(LoginRequiredMixin, generic.TemplateView):
+    """Загальна сторінка щоденника: зведена таблиця оцінок усіх учнів."""
+    template_name = 'diary/overview.html'
+    login_url = 'accounts:login'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        subjects = list(Subject.objects.all())
+        students = StudentProfile.objects.select_related('user')
+
+        averages = {
+            (row['student_id'], row['subject_id']): row['avg']
+            for row in Grade.objects.values('student_id', 'subject_id').annotate(avg=Avg('value'))
+        }
+
+        table = []
+        for student in students:
+            row_grades = [averages.get((student.id, subject.id)) for subject in subjects]
+            table.append({'student': student, 'grades': row_grades})
+
+        context['subjects'] = subjects
+        context['table'] = table
+        return context
+
+
+class StudentGradesView(LoginRequiredMixin, generic.DetailView):
+    """Детальна сторінка з усіма оцінками одного учня."""
+    model = StudentProfile
+    template_name = 'diary/student_detail.html'
+    context_object_name = 'student'
+    login_url = 'accounts:login'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['grades'] = self.object.grades.select_related('subject', 'given_by')
+        return context
